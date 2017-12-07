ba33a3d0351d32f6981931f38e5e554db0235eca
ykhs <yukihisa.yamada@gmail.com>
2017-12-07 19:23:07 +0900

    Create HomeComponent

--- apps/routing-app/src/app/app.module.ts
+++ apps/routing-app/src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [
-  BrowserModule,
-  NxModule.forRoot()],
-  declarations: [AppComponent],
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
